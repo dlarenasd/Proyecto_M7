0a25be2b6169506ba6cc0a6ab20c04ec31554192
dlarenasd <d.larenas.d@gmail.com>
2024-07-05 11:00:40 -0400

    Models, conexión a PostgreSQL y services (CRUD)

---
+++ arriendos/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    numeroRomano = models.CharField(max_length=25, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE, null=False, blank=False, related_name='regiones')
+    
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=False, blank=False, related_name='comunas')
+
+class Direccion(models.Model):
+    calle = models.CharField(max_length=50, null=False, blank=False)
+    numero = models.CharField(max_length=50, null=False, blank=False)
+    depto = models.CharField(max_length=50, null=True, blank=True)
+    comuna = models.ForeignKey(Comuna, null=False, blank=False, related_name='direcciones',  on_delete=models.CASCADE)
+    indicaciones = models.TextField(null=True, blank=True)
+    
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    descripcion = models.TextField(null=True, blank=True)
+
+class Usuario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rut = models.CharField(max_length=12, primary_key=True)
+    direccion = models.CharField(max_length=100, null=False, blank=False)
+    telefono = models.CharField(max_length=15)
+    tipo_usuario = models.ForeignKey(TipoUsuario, null=False, blank=False, related_name='usuarios',  on_delete=models.CASCADE)
+
+class TipoInmueble(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    descripcion = models.TextField(null=True, blank=True)
+
+class Inmueble(models.Model):
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    descripcion = models.TextField()
+    m2_construidos = models.FloatField()
+    m2_terreno = models.FloatField()
+    estacionamientos = models.IntegerField()
+    habitaciones = models.IntegerField()
+    banios = models.IntegerField()
+    direccion = models.OneToOneField(Direccion, unique=True, null=False, on_delete=models.CASCADE)
+    tipo_inmueble = models.ForeignKey(TipoInmueble, null=False, blank=False, related_name='inmuebles',  on_delete=models.CASCADE)
+    precio_mensual = models.IntegerField(null=False, blank=False)
+    arrendador = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
+    arrendatario = models.OneToOneField(Usuario, null=True, blank=True,  on_delete=models.CASCADE)
+    arrendada = models.BooleanField(default=False)
+
+""" 
+class Solicitud(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    usuario = models.OneToOneField(Usuario, null=False, blank=False)
+    inmueble = models.OneToOneField(Inmueble, null=False, blank=False)
+    mensaje = models.TextField(null=False, default='¡Estoy interesado!')
+
+class SolicitudForm(ModelForm):
+    class Meta:
+        model = Solicitud
+        fields = ['fecha', 'usuario', 'inmueble', 'mensaje']
+"""
+
---
+++ arriendos/services.py
@@ -0,0 +1,123 @@
+from django.contrib.auth.models import User
+from .models import Comuna, Direccion, Inmueble, TipoInmueble, Usuario, TipoUsuario
+#CREAR (CREATE)
+
+def crear_direccion(pCalle, pNumero, comuna_id, pIndicaciones, pDepto = None):
+    obj_comuna = Comuna.objects.get(pk = comuna_id)
+    direccion = Direccion(calle = pCalle, 
+                        numero = pNumero, 
+                        comuna = obj_comuna,
+                        indicaciones = pIndicaciones,
+                        depto = pDepto)
+    direccion.save()
+
+def crear_tipo_inmueble(pNombre, pDescripcion):
+    tipo_inmueble = TipoInmueble(nombre = pNombre, 
+                                descripcion = pDescripcion)
+    tipo_inmueble.save()
+    
+def crear_inmueble(pNombre, pDescripcion, pConstruidos, pTerreno, pEstacionamientos, pHabitaciones, pBanios, direccion_id, tipo_id, pPrecio, arrendador_id, pArrendatario = None):
+    obj_direccion = Direccion.objects.get(pk=direccion_id)
+    obj_tipo_inmueble = TipoInmueble.objects.get(pk = tipo_id)
+    obj_arrendador = User.objects.get(pk=arrendador_id)
+    
+    inmueble = Inmueble(nombre = pNombre, 
+                        descripcion = pDescripcion,
+                        m2_construidos = pConstruidos,
+                        m2_terreno = pTerreno,
+                        estacionamientos = pEstacionamientos,
+                        habitaciones = pHabitaciones,
+                        banios = pBanios,
+                        direccion = obj_direccion,
+                        tipo_inmueble = obj_tipo_inmueble,
+                        precio_mensual = pPrecio,
+                        arrendador = obj_arrendador,
+                        arrendatario = pArrendatario)
+    inmueble.save()
+
+def crear_tipo_usuario(pNombre, pDescripcion):
+    tipo_usuario = TipoUsuario(nombre = pNombre, 
+                                descripcion = pDescripcion)
+    tipo_usuario.save()
+    
+def crear_usuario(user_id, pRut, pDireccion, pTelefono, tipo_id):
+    obj_user = User.objects.get(pk=user_id)
+    obj_tipo_usuario = TipoUsuario.objects.get(pk=tipo_id)
+    
+    usuario = Usuario(user = obj_user,
+                    rut = pRut,
+                    direccion = pDireccion, 
+                    telefono = pTelefono,
+                    tipo_usuario = obj_tipo_usuario)
+    usuario.save()
+
+#LISTAR (READ)
+
+def listar_direcciones():
+    lista_direcciones = Direccion.objects.all()
+    for direccion in lista_direcciones :
+        print(direccion)
+
+def listar_inmuebles():
+    lista_inmuebles = Inmueble.objects.all()
+    for inmueble in lista_inmuebles :
+        print(inmueble)
+
+def listar_usuario():
+    lista_usuarios = Usuario.objects.all()
+    for usuario in lista_usuarios :
+        print(usuario)
+
+def listar_tipo_inmuebles():
+    lista_tipos_inmuebles = TipoInmueble.objects.all()
+    for tipo in lista_tipos_inmuebles :
+        print(tipo)
+
+def listar_tipo_usuarios():
+    lista_tipos_usuarios = TipoUsuario.objects.all()
+    for tipo in lista_tipos_usuarios :
+        print(tipo)
+
+#EDITAR(UPDATE)
+
+def editar_direccion(direccion_id, pCalle, pNumero, comuna_id, pIndicaciones, pDepto = None):
+    direccion = Direccion.objects.get(pk=direccion_id)
+    crear_direccion(pCalle, pNumero, comuna_id, pIndicaciones, pDepto)
+    direccion.save()
+
+def editar_inmueble(inmueble_id, pNombre, pDescripcion, pConstruidos, pTerreno, pEstacionamientos, pHabitaciones, pBanios, direccion_id, tipo_id, pPrecio, arrendador_id, pArrendatario = None):
+    inmueble = Inmueble.objects.get(pk=inmueble_id)
+    crear_inmueble(pNombre, pDescripcion, pConstruidos, pTerreno, pEstacionamientos, pHabitaciones, pBanios, direccion_id, tipo_id, pPrecio, arrendador_id, pArrendatario = None)
+    inmueble.save()
+
+def editar_usuario(usuario_id, user_id, pRut, pDireccion, pTelefono, tipo_id):
+    usuario = Usuario.objects.get(pk = usuario_id)
+    crear_usuario(user_id, pRut, pDireccion, pTelefono, tipo_id)
+    usuario.save()
+
+def editar_tipo_inmueble(tipo_id, pNombre, pDescripcion):
+    tipo_inmueble = TipoInmueble.objects.get(pk = tipo_id)
+    crear_tipo_inmueble(pNombre, pDescripcion)
+    tipo_inmueble.save()
+
+def editar_tipo_usuario(tipo_id, pNombre, pDescripcion):
+    tipo_usuario = TipoUsuario.objects.get(pk = tipo_id)
+    crear_tipo_usuario(pNombre, pDescripcion)
+    tipo_usuario.save()
+
+#ELIMINAR (DELETE)
+
+def eliminar_direccion(direccion_id):
+    Direccion.objects.get(pk=direccion_id).delete()
+
+def eliminar_inmueble(inmueble_id):
+    Inmueble.objects.get(pk=inmueble_id).delete()
+
+def eliminar_usuario(usuario_id):
+    Usuario.objects.get(pk=usuario_id).delete()
+
+def eliminar_tipo_inmueble(tipo_id):
+    TipoInmueble.objects.get(pk=tipo_id).delete()
+
+def eliminar_tipo_usuario(tipo_id):
+    TipoUsuario.objects.get(pk=tipo_id).delete()
